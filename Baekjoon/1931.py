da65bcd571f1c211c8eb6c942448ed99c336c0c0
hykhhijk <hjhhijk@gmail.com>
2021-07-25 22:53:38 +0900

    Update 7/25

--- Baekjoon/1931.py
+++ Baekjoon/1931.py
@@ -3,18 +3,21 @@ n = int(input())
 time=[]
 result = 0
 index = 0
+j=0
 for i in range(n):
+    time_temp = []
     start, end = map(int, input().split())
-    time.append([start, end])
+    time.append([start,end])
 
+#파이썬 2차원리스트 리스트 내부 원소 값 정렬예시
+#예에서 두번째 원소를 정렬하였는데 이 전에 미리 sort()를 함으로써 두번째 원소 값이 같을 시 첫번째 원소를 기준으로 정렬하였음
 time.sort()
-print(time)
-for j in range(n):
-    min = 10000000000000
-    for i in range(index, n-1, 1):
-        if(min > time[i][1]):
-            min= time[i][1]
-            index=i
-            result += 1
-        
+time = sorted(time, key=lambda x:x[1])
+
+for i in range(len(time)):
+    if(index <= time[i][0]):
+        index = time[i][1]
+        result += 1
+
 print(result)
+
---
+++ Implementation/Travel.py
@@ -0,0 +1,38 @@
+import time
+
+start = time.time()
+n = int(input())
+
+row = 1
+col = 1
+
+
+
+direction = list(map(str, input().split()))
+
+for i in range(len(direction)):
+    if(direction[i] == "R"):
+        col += 1
+        if(col == n + 1):
+            col -= 1
+    elif(direction[i] == "L"):
+        col -= 1
+        if(col == 0):
+            col += 1
+    elif(direction[i] == "U"):
+        row -= 1
+        if(row == 0):
+            row += 1
+    elif(direction[i] == "D"):
+        row += 1
+        if(row == n + 1):
+            row -= 1
+
+
+
+
+
+
+print(row, col)
+
+print("Time: ", time.time() - start)
