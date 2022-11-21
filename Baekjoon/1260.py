15a631331245b6d8e5efa618e163b0cd05921655
hykhhijk <hjhhijk@gmail.com>
2022-11-21 16:11:17 +0900

    Practice searching

--- Baekjoon/1260.py
+++ Baekjoon/1260.py
@@ -1,42 +1,38 @@
 from collections import deque
+import sys
 
-n, m, k = map(int, input().split())
+node, edge, start = map(int, input().split())
+graph = [[]for _ in range(node+1)]
+visited = [False] * (node+1)
+for _ in range(edge):
+    n,m = map(int,sys.stdin.readline().split())
+    graph[n].append(m)
+    graph[m].append(n)
 
-graph = [[] for _ in range(n+1)]
 
-for _ in range(m):
-    start, end = map(int, input().split())
-    graph[start].append(end)
-    graph[end].append(start)
-for i in range(n+1):
-    graph[i].sort()
-visited = [False]*(n+1)
-temp = deque()
 
-def dfs(i):
-    visited[i] = True
-    print(i, end=" ")
-    if not graph[i]:
-        return False
-    for i in graph[i]:
-        if visited[i] == False:
-            dfs(i)
+def dfs(graph, start, visited):
+    visited[start] = True
+    print(start, end=" ")
+    if len(graph[start]) > 1: graph[start].sort()
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-def bfs(i):
-    print(i, end=" ")
-    if not graph[i]:
-        return False
-    for i in graph[i]:
-        if visited[i] == False:
-            temp.append(i)
-            visited[i] = True
-    if not temp:
-        return False
-    bfs(temp.popleft())
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        node= queue.popleft()
+        print(node, end=" ")
+        if len(graph[node]) > 1: graph[node].sort()
+        for i in graph[node]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i]=True
 
- 
-dfs(k)
-print("")
-visited = [False]*(n+1)
-visited[k]=True
-bfs(k)
+
+dfs(graph, start, visited)
+print()
+visited = [False] * (node+1)
+bfs(graph, start, visited)
