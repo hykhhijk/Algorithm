4362871053e60ff2bf3db01cdcd32c871ff7c4be
hykhhijk <hjhhijk@gmail.com>
2022-12-18 16:01:08 +0900

    Stack

--- Baekjoon/17298.py
+++ Baekjoon/17298.py
@@ -1,21 +1,20 @@
 n = int(input())
 
 num_list = list(map(int, input().split()))
-# num_list = num_list.reverse()
-num_list.reverse()
-big_list=[]
+stack = []
 result_list=[]
-for i in num_list:
-    while big_list:
-        if big_list[-1] <= i:
-            big_list.pop()
-        else:
-            result_list.append(big_list[-1])
-            break
-    
-    if len(big_list)==0:
-        result_list.append(-1)
-    big_list.append(i)
 
+while num_list:
+    num = num_list.pop()
+    while stack:
+        if num >= stack[-1]:
+            stack.pop()
+        else:
+            result_list.append(stack[-1])
+            stack.append(num)
+            break
+    else:
+        result_list.append(-1)
+        stack.append(num)
 result_list.reverse()
-print(" ".join(map(str, result_list)))
+print(" ".join(map(str, (result_list))))
