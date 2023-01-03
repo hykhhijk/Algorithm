5456e33284541312721f998237c0d1725f82bf87
hykhhijk <hjhhijk@gmail.com>
2023-01-03 15:38:05 +0900

    DP

--- Baekjoon/2133_.py
+++ Baekjoon/2133_.py
@@ -6,8 +6,9 @@ for i in range(len(dp)):
         dp[i] = 3
     elif i==4:
         dp[i] = 11
-    elif i > 5:
-        dp[i] = dp[i-2]*3 + 2
+    elif i>5 and i%2==0:
+        dp[i] = dp[i-2]*3
         for j in range(4, i+1, 2):
             dp[i] += dp[i-j]*2
+        dp[i] +=2
 print(dp[n])
